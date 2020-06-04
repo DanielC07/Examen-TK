87874a7ea7f61240557863d03793a5472c954bd9
Daniel Cabrera <danielcd5769@gmail.com>
2020-06-04 00:35:30 -0600

    Se agregaron los cuadros de texto

--- examen.py
+++ examen.py
@@ -4,6 +4,15 @@ window = tkinter.Tk()
 window.geometry("800x300")
 
 label1 = tkinter.Label(window, text = "Nombre")
-label1.pack()
+label1 = tkinter.Label(window, text = "Apellido")
+label1 = tkinter.Label(window, text = "Día")
+label1 = tkinter.Label(window, text = "Mes")
+label1 = tkinter.Label(window, text = "Año")
+
+txt1 = tkinter.Entry(window)
+txt2 = tkinter.Entry(window)
+txt3 = tkinter.Entry(window)
+txt4 = tkinter.Entry(window)
+txt5 = tkinter.Entry(window)
 
 window.mainloop()
